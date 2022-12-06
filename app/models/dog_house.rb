72f81e7ba679db86d7c8fdc0246bf9dbd1fda8c5
Ashley <ashleynjeri7@gmail.com>
2022-12-06 12:42:20 +0300

    dependent: :destroy added

--- app/models/dog_house.rb
+++ app/models/dog_house.rb
@@ -1,3 +1,3 @@
 class DogHouse < ApplicationRecord
-  has_many :reviews
+  has_many :reviews, dependent: :destroy # tells Active Record to delete all the associated records when the parent record is deleted
 end
